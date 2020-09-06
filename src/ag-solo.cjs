4236fd0cc0260bae9105032af30a688a3deddb95
Michael FIG <mfig@agoric.com>
2020-09-05 23:52:58 -0600

    fix: almost building from NPM sources

--- src/ag-solo.cjs
+++ src/ag-solo.cjs
@@ -1,4 +1,15 @@
 // ag-solo entrypoint
 const esmRequire = require('esm')(module);
 
+// If you see:
+// /Users/michael/agoric/agoric-sdk/node_modules/depd/index.js:1
+// TypeError: callSite.getFileName is not a function
+//     at callSiteLocation (/Users/michael/agoric/agoric-sdk/node_modules/depd/index.js:252:23)
+//     at depd (/Users/michael/agoric/agoric-sdk/node_modules/depd/index.js:111:14)
+//     at Object.<anonymous> (/Users/michael/agoric/agoric-sdk/node_modules/body-parser/index.js:14:32)
+//     at Object.Module._extensions..js (internal/modules/cjs/loader.js:1004:10)
+//
+// then you need the depd patch with:
+// cd agoric-sdk
+// git revert 354a3aecc3c26f06d90d5648201ad32f4269b51a
 esmRequire('@agoric/cosmic-swingset/lib/ag-solo/entrypoint.js');
