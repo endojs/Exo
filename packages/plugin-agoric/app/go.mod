2d9bea5e48a8e3f729ad52c1706a829e1b8d2174
Michael FIG <mfig@agoric.com>
2021-02-21 18:05:54 -0600

    feat: add ledger support

--- packages/plugin-agoric/app/go.mod
+++ packages/plugin-agoric/app/go.mod
@@ -1,6 +1,12 @@
-module github.com/agoric-labs/Pledger/packages/plugin-agoric/app
+module main
 
 go 1.15
+
+require (
+	github.com/Agoric/agoric-sdk v0.24.0
+	github.com/cosmos/cosmos-sdk v0.40.1 // indirect
+	github.com/ethereum/go-ethereum v1.9.22 // indirect
+)
 
 // Silence a warning on MacOS
 replace github.com/keybase/go-keychain => github.com/99designs/go-keychain v0.0.0-20191008050251-8e49817e8af4
@@ -13,5 +19,3 @@ replace github.com/gogo/protobuf => github.com/regen-network/protobuf v1.3.2-alp
 // At least until https://github.com/cosmos/cosmos-sdk/issues/8478 is solved and
 // released.
 replace github.com/cosmos/cosmos-sdk => github.com/agoric-labs/cosmos-sdk v0.34.4-0.20210129184725-f1afab29a888
-
-require github.com/Agoric/agoric-sdk v0.0.0-20210217030458-3169872b4b29 // indirect
--- packages/plugin-agoric/app/scripts/build-sdk.cjs
+++ packages/plugin-agoric/app/scripts/build-sdk.cjs
@@ -3,23 +3,26 @@ const { spawnSync } = require('child_process');
 const path = require('path');
 const fs = require('fs');
 
+// process.env.GOPATH = `${process.cwd()}/gopath`;
+// process.env.GO111MODULE = 'on';
+
 function linkAllElectrons() {
-for (const [_src, dst] of [
-	['../../../../node_modules/electron', 'node_modules/electron'],
-	['../../../node_modules/electron', '../../app-electron/node_modules/electron']
-]) {
-	const src = `${__dirname}/../../../../node_modules/electron`;
-	console.log('linking', src, 'to', dst);
-	try {
-		fs.unlinkSync(dst);
-	} catch (e) {
-		if (e.code !== 'ENOENT') {
-			throw e;
-		}
-	}
-	fs.mkdirSync(path.dirname(dst), { recursive: true });
-	fs.symlinkSync(src, dst, 'junction');
-}
+  for (const [_src, dst] of [
+    ['../../../../node_modules/electron', 'node_modules/electron'],
+    ['../../../node_modules/electron', '../../app-electron/node_modules/electron']
+  ]) {
+    const src = `${__dirname}/../../../../node_modules/electron`;
+    console.log('linking', src, 'to', dst);
+    try {
+      fs.unlinkSync(dst);
+    } catch (e) {
+      if (e.code !== 'ENOENT') {
+        throw e;
+      }
+    }
+    fs.mkdirSync(path.dirname(dst), { recursive: true });
+    fs.symlinkSync(src, dst, 'junction');
+  }
 }
 linkAllElectrons();
 
@@ -28,29 +31,53 @@ try {
   fs.renameSync('node_modules/morgan/node_modules/depd', 'node_modules/morgan/node_modules/depd.old');
 } catch (e) {
   if (e.code !== 'ENOENT') {
-   throw e;
+    throw e;
   }
 }
 
-const css = path.dirname(require.resolve('@agoric/cosmic-swingset/package.json'));
 const pjson = require('@agoric/cosmic-swingset/package.json');
+const goMod = `github.com/Agoric/agoric-sdk`;
 
 // Note that the version.Name must be alphanumeric only.
 // Otherwise, generated "os" keyrings on Ubuntu 20.04 can't be read.
 const VersionName = pjson.name.replace(/[^A-Za-z0-9]/g, '');
 
+function fetchGitHead(gitHead) {
+  if (!gitHead) {
+    throw Error(`Specify git head to fetch`);
+  }
+
+  console.log(`Go getting ${goMod}@${gitHead}`);
+  const goGet = spawnSync('go', ['get', '-v', `${goMod}@${gitHead}`],
+    {
+      stdio: ['inherit', 'inherit', 'inherit'],
+    });
+  if (goGet.error) {
+    throw goGet.error;
+  }
+}
+
+const gitHead = process.argv[2] || pjson.gitHead;
+fetchGitHead(gitHead);
+const build_tags = 'ledger';
+const build_tags_comma_sep = build_tags.replace(/ +/, ',');
+
 console.log('Compiling bin/ag-cosmos-helper with Go...');
-const ret = spawnSync('go', ['build', '-v', '-ldflags',`\
+const ret = spawnSync('go', ['build', '-v', '-ldflags', `\
  -X github.com/cosmos/cosmos-sdk/version.Name=${VersionName}\
- -X github.com/cosmos/cosmos-sdk/version.AppName=ag-chain-cosmos\
- -X github.com/cosmos/cosmos-sdk/version.Version=${pjson.version}`,
- '-buildmode=exe',
- '-o', `${__dirname}/../bin/ag-cosmos-helper`,
- 'github.com/Agoric/agoric-sdk/golang/cosmos/cmd/helper'],
- {
-//	 cwd: css,
-	 stdio: ['inherit', 'inherit', 'inherit'],
- },
+ -X github.com/cosmos/cosmos-sdk/version.AppName=ag-cosmos-server\
+ -X github.com/cosmos/cosmos-sdk/version.Version=${pjson.version}\
+ -X github.com/cosmos/cosmos-sdk/version.Commit=${gitHead}\
+ -X "github.com/cosmos/cosmos-sdk/version.BuildTags=${build_tags_comma_sep}"\
+`,
+  '-buildmode=exe', '-tags', build_tags,
+  '-o', `${__dirname}/../bin/ag-cosmos-helper`,
+  `${goMod}/golang/cosmos/cmd/helper`
+  ],
+  {
+    //	 cwd: css,
+    stdio: ['inherit', 'inherit', 'inherit'],
+  },
 );
 
 if (ret.error) {
