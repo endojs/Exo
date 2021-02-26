62abe484cd63958a03004ffcec7aaeb641f191cb
Michael FIG <mfig@agoric.com>
2021-02-26 11:14:25 -0600

    fix: don't exit if dashboard is closed without quitting

--- packages/app-electron/src/main.js
+++ packages/app-electron/src/main.js
@@ -120,14 +120,19 @@ async function main(args, isProduction) {
   // app.on('ready', createWindow);
   app.on('ready', createTray);
 
-  // Quit when all windows are closed, except on macOS. There, it's common
-  // for applications and their menu bar to stay active until the user quits
+  // Quit when all windows are closed, except on macOS. There, it's common for
+  // applications and their menu bar to stay active until the user quits
   // explicitly with Cmd + Q.
+  //
+  // NOTE(michaelfig): We don't do this because we want the system tray to stay
+  // alive on all platforms, unless explicitly quit.
+  /*
   app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
       app.quit();
     }
   });
+  */
 
   app.on('activate', () => {
     // On OS X it's common to re-create a window in the app when the
